6a2e72f8c44f6f07b0c22775c28d39f3fcec927f
Shamik Bera <berashamik115@gmail.com>
2026-03-19 18:18:41 +0530

    chore: add ageDistribution as prop for AnalyticsSummary type;

--- src/types/analytics.ts
+++ src/types/analytics.ts
@@ -2,5 +2,6 @@ export interface AnalyticsSummary {
   totalPatients: number
   averageAge: number
   totalReports: number
+  ageDist: Record<string, number>
   reportStatusDist: Record<string, number>
 }
