0dbd81938f9cea3d208afcb5d643e595cb25b4ff
Shamik Bera <berashamik115@gmail.com>
2026-03-19 17:44:52 +0530

    chore: add ReportListView Component export to common barrel export;

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export { default as PatientGridView } from './PatientGridView'
 export { default as PatientListView } from './PatientListView'
+export { default as ReportListView } from './ReportListView'
